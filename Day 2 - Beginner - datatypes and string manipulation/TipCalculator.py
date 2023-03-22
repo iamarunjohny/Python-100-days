01ee55ac592b73808c5f244972fa88f2ad605d7c
Arun <arunjohny74@gmail.com>
2023-03-22 18:50:28 +0530

    TipCalculator deleted

--- Day 2 - Beginner - datatypes and string manipulation/TipCalculator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to tip calculator")
-bill = input("What was the bill? \n")
-tip = input("What percentage tip would you like to give? 10, 12, or 15? \n")
-noOFpeople = input ("How many people to split the bill? \n")
-percent = ((float(tip)/100)*float(bill ))
-total = percent + float(bill)
-billWithTip = total/float(noOFpeople)
-print(f"Each person should pay : {round(billWithTip)} ")
